23ed53389f34407c01cf07ad405530867b7ff6e5
orodriguez <orodriguez@veracode.com>
2021-02-17 15:57:33 -0500

    New file test3.py

---
+++ test3.py
@@ -0,0 +1,22 @@
+import sys
+import json
+import requests
+
+myUrl='https://dev.azure.com/oscarrodriguezarias/PipelineScan2AzureWorkItems/_apis/wit/workitems/${type}?api-version=6.0'
+myHeader={'Content-Type': 'application/json-patch+json'}
+data=[
+    {
+        "op": "add",
+        "path": "/fields/System.Title",
+        "value": "Sample Task"
+    }
+]
+
+def createWorkItem():
+    myResponse = requests.post(myUrl,json=data,headers=myHeader)
+    print(myResponse.json())
+
+def main():
+    createWorkItem()
+
+main()
