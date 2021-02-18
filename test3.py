66d8a4ceb64fa69acfbffa7108101d3628bf213b
orodriguez <orodriguez@veracode.com>
2021-02-18 12:38:06 -0500

    askjdls

--- test3.py
+++ test3.py
@@ -1,9 +1,19 @@
 import sys
 import json
 import requests
+import argparse
+
+parser = argparse.ArgumentParser(description='Accept flags from CLI')
+parser.add_argument('-t', action='store', help='Azure Pipeline Token', type=str)
+args = parser.parse_args()
+
+if str(args.t) == "None":
+    sys.exit("ERROR. A token is required!")
+else:
+    myToken = str(args.t)
 
 myUrl='https://dev.azure.com/oscarrodriguezarias/PipelineScan2AzureWorkItems/_apis/wit/workitems/${type}?api-version=6.0'
-myHeader={'Content-Type': 'application/json-patch+json'}
+myHeader={'Content-Type': 'application/json-patch+json', 'Authorization': 'Bearer ' + myToken}
 data=[
     {
         "op": "add",
@@ -13,7 +23,7 @@ data=[
 ]
 
 def createWorkItem(myToken):
-    myResponse = requests.post(myUrl,json=data,headers=myHeader,auth=('',myToken))
+    myResponse = requests.post(myUrl,json=data,headers=myHeader)
     #print(myResponse.json())
     print(myResponse.text)
 
