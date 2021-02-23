378627262d404834af91c4fe6e70fad320d57c3b
orodriguez <orodriguez@veracode.com>
2021-02-23 09:23:14 -0500

    Adjusted

--- Pipeline2AzureWorkItems.py
+++ Pipeline2AzureWorkItems.py
@@ -158,14 +158,13 @@ def preparerequestbody(cwe_id, severity_name, issue_type, code_line, file_name, 
 
 
 def createworkitem(cwe_id, severity_name, issue_type, code_line, file_name, scope, issue_id):
-    try:
-        # INCLUDE HERE API CALL TO CREATE WORK ITEM IN AZURE DEVOPS
-        reqBody = preparerequestbody(cwe_id, severity_name, issue_type, code_line, file_name, scope, issue_id)
-        myResponse = requests.post(myUrl, json=reqBody, headers=myHeader)
-        myText = myResponse.status_code + ' - ' + 'Work item was created successfully!'
-        print(myText)
-    except:
-        sys.exit("Error while creating work item in Azure DevOps!")
+#    try:
+    reqBody = preparerequestbody(cwe_id, severity_name, issue_type, code_line, file_name, scope, issue_id)
+    myResponse = requests.post(myUrl, json=reqBody, headers=myHeader)
+    myText = myResponse.status_code + ' - ' + 'Work item was created successfully!'
+    print(myText)
+#    except:
+#        sys.exit("Error while creating work item in Azure DevOps!")
 
 
 def main():
